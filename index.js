c7ea5598b1dc3bbec969f113fef5fa1c0051daef
alu0100782851 <alu0100782851@ull.edu.es>
2016-10-10 21:36:56 +0000

    update

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/gh-pages/gitbook'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/gh-pages');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
+
+
 app.get('/', function(request, response) {
   response.render('index.html');
 });
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port:', app.get('port'));
