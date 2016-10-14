1ad4e3d70b3bb1d6f103e88314affcb2003cee75
alu0100782851 <alu0100782851@ull.edu.es>
2016-10-14 07:53:14 +0000

    actualizando index

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (request, response) => {
 
 
 app.post('/synchronize', (request, response) => {
-  proces.exec('git pull git@github.com:ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git master',
+  proces.exec('git pull',
     function (err,stdout,stderr) {
       if (err) {
           console.log("\n"+stderr);
