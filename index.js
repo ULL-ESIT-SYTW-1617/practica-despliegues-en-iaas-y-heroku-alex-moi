59bc793d2f754f4064c28ba06c51495eddfbfea1
alu0100782851 <alu0100782851@ull.edu.es>
2016-10-14 17:15:25 +0000

    Prueba Heroku

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/synchronize', (request, response) => {
     fs.existsSync(path.resolve(__dirname, 'gitbook-alex-moi-nitesh')) ? pull() : clone();
     
     function pull() { 
-      
+      proces.exec('git init');
       proces.exec('git pull git@github.com:ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git',
       function (err,stdout,stderr) {
         if (err) {
