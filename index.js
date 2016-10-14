67a99435ee4d51bbdbe8f65a8d67e38f3a461f3b
alu0100782851 <alu0100782851@ull.edu.es>
2016-10-14 17:35:23 +0000

    Prueba Heroku

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ app.get('/synchronize', (request, response) => {
 });
 
 
-app.post('/synchronize', (request, response) => {
+app.get('/synchronizee', (request, response) => {
     fs.existsSync(path.resolve(__dirname, 'gitbook-alex-moi-nitesh')) ? pull() : clone();
     
     function pull() { 
-       response.send("Sincronizando 1");
-      proces.exec('cd app/gitbook-alex-moi-nitesh');
+      response.send("Sincronizando 1");
+      proces.exec('cd ' + path.resolve(__dirname, 'gitbook-alex-moi-nitesh') );
       proces.exec('git pull git@github.com:ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git',
       function (err,stdout,stderr) {
         if (err) {
@@ -48,13 +48,13 @@ app.post('/synchronize', (request, response) => {
     
     function clone() { 
       response.send("Sincronizando 2");
-      proces.exec('cd app/gitbook-alex-moi-nitesh');
+      proces.exec('cd ' + path.resolve(__dirname, 'gitbook-alex-moi-nitesh'));
       proces.exec('git clone git@github.com:ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git',
       function (err,stdout,stderr) {
         if (err) {
             console.log("\n"+stderr);
         } else {
-            console.log("Git Pull: " + stdout);
+            console.log("Git Clone: " + stdout);
             response.send("Salida: " + stdout);
         }
       });
