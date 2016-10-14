5b16ec75fcbb985cd2055a33e20cef6388cff220
alu0100782851 <alu0100782851@ull.edu.es>
2016-10-14 17:06:42 +0000

    Heroku funciona por favor

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 var express = require('express');
 var app = express();
 var proces = require('child_process');
+var fs = require('fs');
+var path = require('path');
 
 
 app.set('port', (process.env.PORT || 8080));
-
 app.use(express.static(__dirname + '/gh-pages/'));
-
-/*// views is directory for all template files
-app.set('views', __dirname + '/gh-pages');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');*/
 
 
 
 app.get('/', (request, response) => {
   response.send('index.html');
 });
+
 
 app.get('/synchronize', (request, response) => {
     proces.exec('git pull git@github.com:ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git',
@@ -31,25 +28,32 @@ app.get('/synchronize', (request, response) => {
     response.redirect('/');
 });
 
+
 app.post('/synchronize', (request, response) => {
-    proces.exec('git clone git@github.com:ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git',
-    function (err,stdout,stderr) {
-      if (err) {
-          console.log("\n"+stderr);
-      } else {
-          console.log("Git Pull: " + stdout);
-          response.send("Salida: " + stdout)
-      }
-    });
-  /*proces.exec('git pull https://github.com/ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git',
-    function (err,stdout,stderr) {
-      if (err) {
-          console.log("\n"+stderr);
-      } else {
-          console.log("Git Pull: " + stdout);
-          response.send("Salida: " + stdout)
-      }
-    });*/
+    fs.existsSync(path.resolve(__dirname, 'gitbook-alex-moi-nitesh')) ? pull() : clone()
+    
+    function pull() { proces.exec('git pull git@github.com:ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git',
+      function (err,stdout,stderr) {
+        if (err) {
+            console.log("\n"+stderr);
+        } else {
+            console.log("Git Pull: " + stdout);
+            response.send("Salida: " + stdout)
+        }
+      });
+    }
+    
+    function clone() { proces.exec('git clone git@github.com:ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git',
+      function (err,stdout,stderr) {
+        if (err) {
+            console.log("\n"+stderr);
+        } else {
+            console.log("Git Pull: " + stdout);
+            response.send("Salida: " + stdout)
+        }
+      });
+    }
+
     response.send("Sincronizando");
 });
 
