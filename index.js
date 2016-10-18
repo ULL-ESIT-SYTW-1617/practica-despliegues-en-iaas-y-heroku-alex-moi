a9a8a234df3848b169e489fbed4120d3d7f04ee9
alu0100782851 <alu0100782851@ull.edu.es>
2016-10-18 17:50:01 +0000

    Cambios

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 var proces = require('child_process');
 var path = require('path');
 var directorio = path.resolve(__dirname, 'gitbook-alex-moi-nitesh');
@@ -30,29 +31,32 @@ app.get('/synchronize', (request, response) => {
 
 app.post('/sync', (request, response) => {
     
+  fs.existsSync(path.resolve(__dirname, 'practica-despliegues-en-iaas-y-heroku-alex-moi')) ? pull(): clone();
 
-  console.log(`Directorio actual: ${process.cwd()}`);
-
-  /*proces.exec('git clone https://github.com/ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git', 
-    function(err,stdout,stderr){
-      if (err) {
+  function clone(){
+    proces.exec('git clone https://github.com/ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git', 
+      function(err,stdout,stderr){
+        if (err) {
+            console.log("error"+ err);
+            console.log("\n"+stderr);
+        } else {
+            console.log("Git Clone: " + stdout);
+        }
+    });
+  }
+  
+  function pull(){
+    proces.exec("git pull https://github.com/ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git master",{cwd:'/app/practica-despliegues-en-iaas-y-heroku-alex-moi'},
+      function(err,stdout,stderr){
+        if (err) {
           console.log("error"+ err);
-          console.log("\n"+stderr);
-      } else {
-          console.log("Git Clone: " + stdout);
-      }
-  });*/
-  proces.exec("git pull https://github.com/ULL-ESIT-SYTW-1617/practica-despliegues-en-iaas-y-heroku-alex-moi.git master",{cwd:'/app/practica-despliegues-en-iaas-y-heroku-alex-moi'},
-    function(err,stdout,stderr){
-      if (err) {
-        console.log("error"+ err);
-          console.log("\n"+stderr);
-      } else {
-          console.log("Git Pull: " + stdout);
-      }
-  });
-  response.send("Sincronizando");
-   
+            console.log("\n"+stderr);
+        } else {
+            console.log("Git Pull: " + stdout);
+        }
+    });
+    response.send("Sincronizando");
+  }
 });
 
 
